73cd2c9d04f537b221489ab82fab349d6d981733
Aishwarya Reddy Challa <aishwarya92c@gmail.com>
2026-04-19 19:29:19 +0200

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        // Binary search
+        int l = 1;
+        int r = Arrays.stream(piles).max().getAsInt();
+        int result = r;
+        while(l<=r) {
+            int k = (l+r)/2;
+
+            int totalHours = 0;
+
+            for(int p : piles) {
+            totalHours += Math.ceil((double) p/k);
+            }
+
+            if(totalHours > h) {
+            l = k+1;
+            } else {
+            result = k;
+            r = k-1;
+            }
+
+        }
+
+        return result;
+        
+        
+    }
+}
